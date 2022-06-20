22b71c6bffd5e1e660b4ba118d85f95f93534050
DESKTOP-VLRBKNJ\Gola <arielgolafer@gmail.com>
2022-06-19 22:30:56 -0300

    Make Router and restructure components folders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+
 function App() {
     return (
         <div className="App">
-            ddd
+            <Router>
+                <Routes>
+
+                    <Route path='/signin' exact />
+
+                    <Route path='/signup' exact />
+
+                    <Route path='/messages' exact />
+
+                    <Route path='/messages/categories' exact />
+
+                    <Route path='/history' exact />
+
+                </Routes>
+            </Router>
         </div>
     );
 }
