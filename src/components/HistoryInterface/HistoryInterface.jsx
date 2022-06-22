4a59353610a0750928a5b03cd716bb60e03c4037
DESKTOP-VLRBKNJ\Gola <arielgolafer@gmail.com>
2022-06-21 21:36:58 -0300

    Copy MenssageInterface to others ones

---
+++ src/components/HistoryInterface/HistoryInterface.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HistoryInterface() {
+    return (
+        <div className="one-container">
+            <div className="two-container tl-background tl-format">ff</div>
+            <div className="two-container tr-background tr-format">ff</div>
+        </div>
+    )
+}
+
+export default HistoryInterface
