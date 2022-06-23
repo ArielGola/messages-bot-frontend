1b6eabb96765fdca7c3a1b6d899e9c8c2aebcfd8
DESKTOP-VLRBKNJ\Gola <arielgolafer@gmail.com>
2022-06-22 23:25:20 -0300

    Make components navigations

--- src/components/HistoryInterface/HistoryInterface.jsx
+++ src/components/HistoryInterface/HistoryInterface.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
 
 function HistoryInterface() {
+
+    let navigate = useNavigate();
+
     return (
         <div className="one-container">
-            <div className="two-container tl-background tl-format">ff</div>
-            <div className="two-container tr-background tr-format">ff</div>
+            <div className="two-container tl-background tl-format">
+                <div className="two-container tl-background tl-format">
+                    <div className="two-container tl-background tl-format">
+                        <div className="sup-bar">
+                            <button class="btn btn-primary ml-3"><i className="fas fa-bars fa-lg"></i></button>
+                        </div>
+                        <div className="comp-content">
+                            hreherhef
+                        </div>
+                        <div className="down-bar">
+                            <button 
+                                class="btn btn-primary ml-3"
+                                onClick={() => navigate('/messages/categories')}
+                            >
+                                Messages
+                            </button>
+                            <button class="btn btn-primary ml-3">Clear History</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="two-container tr-background tr-format">
+
+            </div>
         </div>
     )
 }
--- src/components/MessagesComponents/MessagesInterface/MessagesInterface.jsx
+++ src/components/MessagesComponents/MessagesInterface/MessagesInterface.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
 
 function MessagesInterface() {
+
+    let navigate = useNavigate();
+
     return (
         <div className="one-container">
-            <div className="two-container tl-background tl-format">ff</div>
-            <div className="two-container tr-background tr-format">ff</div>
+            <div className="two-container tl-background tl-format">
+                <div className="sup-bar">
+                    <button class="btn btn-primary ml-3"><i className="fas fa-bars fa-lg"></i></button>
+                </div>
+                <div className="comp-content">
+                    hreherhef
+                </div>
+                <div className="down-bar">
+                    <button class="btn btn-primary ml-3">New Message</button>
+                    <button 
+                        class="btn btn-primary ml-3"
+                        onClick={() => navigate('/messages/categories')}
+                    >
+                        Categories
+                    </button>
+                    <button 
+                        class="btn btn-primary ml-3"
+                        onClick={() => navigate('/history')}
+                    >
+                        Show History
+                    </button>
+                </div>
+            </div>
+            <div className="two-container tr-background tr-format">
+
+            </div>
         </div>
     )
 }
