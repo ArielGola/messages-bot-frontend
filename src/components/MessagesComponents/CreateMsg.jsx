e8c92b4f560c9c4628a9990b38b55fece9fbc6c7
DESKTOP-VLRBKNJ\Gola <arielgolafer@gmail.com>
2022-07-01 00:42:32 -0300

    Make functionallity os CreateMsg, but went wrong

--- src/components/MessagesComponents/CreateMsg.jsx
+++ src/components/MessagesComponents/CreateMsg.jsx
@@ -1,16 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react';
+import Axios from 'axios';
+
+const CREATEMSG_URL = 'http://localhost:4000/mba/message/create';
 
 function CreateMsg(props) {
+
+    const [NumSend, setNumSend] = useState("");
+    const [Time, setTime] = useState("");
+    const [Text, setText] = useState("");
+    const [Category, setCategory] = useState("");
+    
+    const [Mon, setMon] = useState(false);
+    const [Tue, setTue] = useState(false);
+    const [Wed, setWed] = useState(false);
+    const [Thu, setThu] = useState(false);
+    const [Fri, setFri] = useState(false);
+    const [Sat, setSat] = useState(false);
+    const [Sun, setSun] = useState(false);
 
     const backToMsgs = (newValue) => {
         const doneBack = props.functionDoneBack;
         doneBack(newValue);
     };
 
-    const createMsgFunction = () => {
-        
-        backToMsgs(true);
+    const createMsgFunction = async () => {
+        try {
+            const newMsg = {
+                numSend: NumSend,
+                content: Text,
+                timeSend: Time,
+                category: Category,
+                frequency: {
+                    Mon,
+                    Tue,
+                    Wed,
+                    Thu,
+                    Fri,
+                    Sat,
+                    Sun
+                }
+            };
 
+            await Axios.post(CREATEMSG_URL, newMsg);
+            
+            backToMsgs(true);
+        } catch (error) {
+            console.log("mal");
+        };
     };
 
     return (
@@ -20,20 +56,35 @@ function CreateMsg(props) {
                 <div className="form-row">  
                     <div class="form-group col-md-6">
                         <label class="col-form-label" for="inputDefault">Message To</label>
-                        <input type="text" class="form-control" placeholder="Message to..." />
+                        <input 
+                            type="text" 
+                            class="form-control" 
+                            placeholder="Message to..." 
+                            onChange={(e) => setNumSend(e.target.value)} 
+                        />
                     </div>
                     <div class="form-group col-md-6">
-                        <label class="col-form-label" for="inputDefault">Date & Time</label>
-                        <input type="time" class="form-control" />
+                        <label class="col-form-label" for="inputDefault">Time</label>
+                        <input type="time" class="form-control" onChange={(e) => setTime(e.target.value)} />
                     </div>
                 </div>
                 <div class="form-group">
                     <label class="col-form-label" for="inputDefault">Text</label>
-                    <textarea className='form-control' rows="3" maxLength="300" placeholder="Text..." />
+                    <textarea 
+                        className='form-control' 
+                        rows="3" 
+                        maxLength="300" 
+                        placeholder="Text..." 
+                        onChange={(e) => setText(e.target.value)}
+                    />
                 </div>
                 <div class="form-group">
                     <label class="col-form-label" for="inputDefault">Category</label>
-                    <select class="form-control" id="exampleFormControlSelect1">
+                    <select 
+                        class="form-control" 
+                        id="exampleFormControlSelect1" 
+                        onChange={(e) => setCategory(e.target.value)}
+                    >
                         <option>Family</option>
                         <option>Friends</option>
                         <option>Coworkers</option>
@@ -46,31 +97,66 @@ function CreateMsg(props) {
                     <label class="col-form-label">Frequency</label>
                     <div className='d-flex justify-content-center'>
                         <div class="form-check form-check-inline mr-3">
-                            <input class="form-check-input" type="checkbox" id="" value="" />
+                            <input 
+                                class="form-check-input" 
+                                type="checkbox" 
+                                id="" 
+                                onChange={(e) => setMon(e.target.checked)}
+                            />
                             <label class="form-check-label" for="">Mon</label>
                         </div>
                         <div class="form-check form-check-inline mr-3">
-                            <input class="form-check-input" type="checkbox" id="" value="" />
+                            <input 
+                                class="form-check-input" 
+                                type="checkbox" 
+                                id="" 
+                                onChange={(e) => setTue(e.target.checked)}
+                            />
                             <label class="form-check-label" for="">Tue</label>
                         </div>
                         <div class="form-check form-check-inline mr-3">
-                            <input class="form-check-input" type="checkbox" id="" value="" />
+                            <input 
+                                class="form-check-input" 
+                                type="checkbox" 
+                                id="" 
+                                onChange={(e) => setWed(e.target.checked)}
+                            />
                             <label class="form-check-label" for="">Wed</label>
                         </div>
                         <div class="form-check form-check-inline mr-3">
-                            <input class="form-check-input" type="checkbox" id="" value="" />
+                            <input 
+                                class="form-check-input" 
+                                type="checkbox" 
+                                id="" 
+                                onChange={(e) => setThu(e.target.checked)}
+                            />
                             <label class="form-check-label" for="">Thu</label>
                         </div>
                         <div class="form-check form-check-inline mr-3">
-                            <input class="form-check-input" type="checkbox" id="" value="" />
+                            <input 
+                                class="form-check-input" 
+                                type="checkbox" 
+                                id="" 
+                                onChange={(e) => setFri(e.target.checked)}
+                            />
                             <label class="form-check-label" for="">Fri</label>
                         </div>
                         <div class="form-check form-check-inline mr-3">
-                            <input class="form-check-input" type="checkbox" id="" value="" />
+                            <input 
+                                class="form-check-input" 
+                                type="checkbox" 
+                                id="" 
+                                onChange={(e) => setSat(e.target.checked)}
+                            />
                             <label class="form-check-label" for="">Sat</label>
                         </div>
                         <div class="form-check form-check-inline mr-3">
-                            <input class="form-check-input" type="checkbox" id="" value="" />
+                            <input 
+                                class="form-check-input" 
+                                type="checkbox" 
+                                id="" 
+                                onChange={(e) => setSun(e.target.checked)}
+                            />
                             <label class="form-check-label" for="">Sun</label>
                         </div>
                     </div>
