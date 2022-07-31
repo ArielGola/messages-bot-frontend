26a892dd50a5ea433656ee433fc4321cf46b63e2
DESKTOP-VLRBKNJ\Gola <arielgolafer@gmail.com>
2022-07-31 11:51:23 -0300

    Solve a warning

--- src/components/MessagesComponents/CategoriesInterface/CategoriesInterface.jsx
+++ src/components/MessagesComponents/CategoriesInterface/CategoriesInterface.jsx
@@ -43,7 +43,6 @@ function CategoriesInterface() {
         };
 
         getMessages();
-
     }, []);
 
     // Workout variables
--- src/components/MessagesComponents/IndividualMsg.jsx
+++ src/components/MessagesComponents/IndividualMsg.jsx
@@ -17,26 +17,24 @@ function IndividualMsg(props) {
     return (
         <div className="msg-w-container mt-2">
             <div className="list-group p-2">
-                <a 
-                    href="#"
+                <div 
                     className="list-group-item list-group-item-action flex-column align-items-start active"
-                    onClick={(e) => {e.preventDefault(); handleSelectedId(MsgData._id)}}
+                    onClick={() => handleSelectedId(MsgData._id)}
                 >
                     <div className="d-flex w-100 justify-content-between">
                         <p className="mb-1">To: {MsgData.numSend}</p>
                         <small>{MsgData.timeSend}</small>
                     </div>
-                </a>
-                <a 
-                    href="#" 
+                </div>
+                <div 
                     className="list-group-item list-group-item-action flex-column align-items-start"
-                    onClick={(e) => {e.preventDefault(); handleSelectedId(MsgData._id)}}
+                    onClick={() => handleSelectedId(MsgData._id)}
                 >
                     <div className="d-flex w-100 justify-content-between">
                         <p className="mb-1">Text: {MsgData.content}</p>
                         <small className="text-muted">{String(MsgData.createdAt).split('T')[0]}</small>
                     </div>
-                </a>
+                </div>
             </div>
         </div> 
     )
